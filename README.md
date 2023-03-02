c62987a326aa93f09c7edacd307f462363a78c38
lashad <lashad@mac.com>
2023-03-02 12:15:37 +0400

    First Draft version

--- README.md
+++ README.md
@@ -1,2 +1,95 @@
-# esp32_uart_bridge
-ESP32-C3 act as USB to serial UART interface like FT232R or CP210x
+| Supported Target | ESP32-C3 |
+| ---------------- | -------- |
+
+# ESP32-C3 UART Bridge
+
+The ESP32-C3 UART Bridge is an ESP-IDF project. This makes the ESP32-C3 act as USB to serial UART interface like FT232R or CP210x.
+The communication is transferred in both directions between the Computer and the target MCU through the ESP32-C3 UART Bridge.
+
+> NOTE: Firmware tested on ESP32-C3. But it may work in other ESP32 family.
+
+See diagram below:
+
+```
+ COMPUTER            ESP32-C3             MCU
+┌────────┐     ┌────────┬────────┐     ┌────────┐
+│        │     │        │        │     │        │
+│        │     │        │        │     │        │
+│        │     │        │        │     │        │
+│        │     │        │        │     │        │
+│  USB   │◄───►│  CDC   │  UART  │◄───►│  UART  │
+│        │     │        │        │     │        │
+│        │     │        │        │     │        │
+│        │     │        │        │     │        │
+│        │     │        │        │     │        │
+└────────┘     └────────┴────────┘     └────────┘
+```
+
+
+### Hardware Required
+
+The example can be run on ESP32-C3 based development board connected to a computer with a single USB cable for flashing and bridging. The target MCU interface should have 3.3V logic. 
+
+### Setup the Hardware
+
+Connect the target MCU serial interface to the ESP32-C3 as follows.
+
+
+-----------------------------------------------------------------------------------------
+| ESP32-C3 chip         | Kconfig Option     | ESP32-C3 Pin | Target MCU UART Pin |
+| ----------------------|--------------------|----------------------|--------------------|
+| Transmit Data (TxD)   | UBRIDGE_UART_TXD | GPIO21             | RxD               |
+| Receive Data (RxD)    | UBRIDGE_UART_RXD | GPIO20         | TxD               |
+| Ground                | n/a                | GND                  | GND               |
+
+See connection diagram below:
+
+```
+ ESP32-C3           MCU
+┌────────┐       ┌────────┐
+│        │       │        │
+│        │       │        │
+│ GPIO21 ├──────►│  RxD   │
+│        │ 3.3v  │        │
+│        │ logic │        │
+│ GPIO20 │◄──────┤  TxD   │
+│        │       │        │
+│        │       │        │
+└────────┘       └────────┘
+```
+
+
+
+## How to Compile the Project
+
+[ESP-IDF](https://github.com/espressif/esp-idf) v5.0.1 or newer can be used to compile the project. Please read the
+documentation of ESP-IDF for setting up the environment.
+
+- `idf.py menuconfig` can be used to change the default configuration. The project-specific settings are in the "ESP32 Uart Bridge Configuration" sub-menu.
+- `idf.py build` will build the project binaries.
+- `idf.py -p PORT flash` will flash the ESP32-C3. The `PORT` is the serial port created by an ESP32-C3 USB-CDC profile connected to the USB D+/D- pins of the ESP32-C3. This serial connection has to be established to upload firmware into the ESP32-C3 and it also used to act as bridge. 
+
+The initial flashing can be done by using other methods, see [this guide](https://docs.espressif.com/projects/esp-idf/en/latest/esp32c3/get-started/establish-serial-connection.html).
+
+### Configure the project
+
+Use the command below to configure project using Kconfig menu as showed in the table above.
+The default Kconfig values can be changed such as: `UBRIDGE_UART_BAUD_RATE`, `UBRIDGE_UART_PORT_NUM` (Refer to Kconfig file).
+
+```
+idf.py menuconfig
+```
+
+> NOTE: The default baud rate is `57600` and port number `0`
+
+### Build and Flash
+
+Build the project and flash it to the board:
+
+```
+idf.py build
+idf.py -p PORT flash
+```
+
+
+
---
+++ main/esp32_uart_bridge.c
@@ -0,0 +1,125 @@
+/*
+ *    esp32_uart_bridge.c
+ *
+ *     ________  ________  ___  ________  ________  _______      
+ *    |\   __  \|\   __  \|\  \|\   ___ \|\   ____\|\  ___ \     
+ *    \ \  \|\ /\ \  \|\  \ \  \ \  \_|\ \ \  \___|\ \   __/|    
+ *     \ \   __  \ \   _  _\ \  \ \  \ \\ \ \  \  __\ \  \_|/__  
+ *      \ \  \|\  \ \  \\  \\ \  \ \  \_\\ \ \  \|\  \ \  \_|\ \ 
+ *       \ \_______\ \__\\ _\\ \__\ \_______\ \_______\ \_______\
+ *        \|_______|\|__|\|__|\|__|\|_______|\|_______|\|_______|
+ *
+ *
+ *    Copyright (c) 2023 Alien Green LLC
+ *
+ *    This file is part of UnitVM.
+ *
+ *    UnitVM is free software: you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation, either version 3 of the License, or
+ *    (at your option) any later version.
+ *
+ *    UnitVM is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *
+ *    You should have received a copy of the GNU General Public License
+ *    along with UnitVM.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *    ASCII font see http://patorjk.com/software/taag/#p=display&f=3D-ASCII
+ */
+
+#include <stdio.h>
+#include <fcntl.h>
+
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/uart.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+#include "hal/usb_serial_jtag_ll.h"
+#include "driver/usb_serial_jtag.h"
+
+#define UBRIDGE_PIN_TXD           CONFIG_UBRIDGE_UART_TXD
+#define UBRIDGE_PIN_RXD           CONFIG_UBRIDGE_UART_RXD
+#define UBRIDGE_PIN_RTS           UART_PIN_NO_CHANGE
+#define UBRIDGE_PIN_CTS           UART_PIN_NO_CHANGE
+
+#define UBRIDGE_UART_PORT_NUM     CONFIG_UBRIDGE_UART_PORT_NUM
+#define UBRIDGE_UART_BAUD_RATE    CONFIG_UBRIDGE_UART_BAUD_RATE
+#define UBRIDGE_TASK_STACK_SIZE   CONFIG_UBRIDGE_TASK_STACK_SIZE
+
+#define BUF_SIZE                  512
+
+
+/* ----------------------------------------------------------- */
+
+static void bridge_task(void *arg)
+{
+
+    /* Configure USB-CDC */
+    usb_serial_jtag_driver_config_t usb_serial_config = {
+        .tx_buffer_size = 128,
+        .rx_buffer_size = 128
+    };
+
+    ESP_ERROR_CHECK(usb_serial_jtag_driver_install(&usb_serial_config));
+
+    /* Configure parameters of an UART driver,
+     * communication pins and install the driver */
+    uart_config_t uart_config = {
+        .baud_rate = UBRIDGE_UART_BAUD_RATE,
+        .data_bits = UART_DATA_8_BITS,
+        .parity    = UART_PARITY_EVEN,
+        .stop_bits = UART_STOP_BITS_1,
+        .flow_ctrl = UART_HW_FLOWCTRL_DISABLE,
+        .source_clk = UART_SCLK_DEFAULT,
+    };
+    int intr_alloc_flags = 0;
+
+#if CONFIG_UART_ISR_IN_IRAM
+    intr_alloc_flags = ESP_INTR_FLAG_IRAM;
+#endif
+
+    // gpio_reset_pin(UBRIDGE_PIN_TXD);
+    // gpio_reset_pin(UBRIDGE_PIN_RXD);
+    // gpio_set_direction(UBRIDGE_PIN_TXD, GPIO_MODE_INPUT);
+    // gpio_set_direction(UBRIDGE_PIN_RXD, GPIO_MODE_INPUT);
+
+    ESP_ERROR_CHECK(uart_driver_install(UBRIDGE_UART_PORT_NUM, BUF_SIZE * 2, 0, 0, NULL, intr_alloc_flags));
+    ESP_ERROR_CHECK(uart_param_config(UBRIDGE_UART_PORT_NUM, &uart_config));
+    ESP_ERROR_CHECK(uart_set_pin(UBRIDGE_UART_PORT_NUM, UBRIDGE_PIN_TXD, UBRIDGE_PIN_RXD, UBRIDGE_PIN_RTS, UBRIDGE_PIN_CTS));
+
+
+    /* Configure a bridge buffer for the incoming data */
+    uint8_t *data = (uint8_t *) malloc(BUF_SIZE);
+
+    while (true) {
+
+        int len = usb_serial_jtag_read_bytes(data, BUF_SIZE, 0 / portTICK_PERIOD_MS);
+        if(len > 0) {
+            uart_write_bytes(UBRIDGE_UART_PORT_NUM, (const char *) data, len);
+            uart_flush(UBRIDGE_UART_PORT_NUM);
+        }
+
+        len = uart_read_bytes(UBRIDGE_UART_PORT_NUM, data, (BUF_SIZE), 0 / portTICK_PERIOD_MS);
+        if(len > 0) {
+            usb_serial_jtag_write_bytes(data, len,  0 / portTICK_PERIOD_MS);
+            usb_serial_jtag_ll_txfifo_flush();
+        }
+
+    }
+
+    /* Never reached */
+    ESP_ERROR_CHECK(uart_driver_delete(UBRIDGE_UART_PORT_NUM));
+}
+
+/* ----------------------------------------------------------- */
+
+void app_main(void) {
+
+    xTaskCreate(bridge_task, "uart_bridge_task", UBRIDGE_TASK_STACK_SIZE, NULL,  2 | portPRIVILEGE_BIT, NULL);
+}
+
+/* ----------------------------------------------------------- */
